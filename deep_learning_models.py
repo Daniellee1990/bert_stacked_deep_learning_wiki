902ce4b13e134cea5cadbe00aa181efb2429333e
xiaodan Li <lidaniel1990@gmail.com>
2019-10-27 11:17:50 -0700

    Add files via upload

---
+++ deep_learning_models.py
@@ -0,0 +1,250 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 23 12:23:54 2018
+
+@author: lixiaodan
+"""
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.optimizers import SGD
+from keras.layers import LSTM, Bidirectional
+from keras.layers.convolutional import Conv1D
+from keras.layers.convolutional import MaxPooling1D
+from keras.layers import Flatten
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, auc
+import itertools
+import numpy as np
+from sklearn.metrics import accuracy_score
+from sklearn import metrics
+from sklearn.metrics import log_loss
+
+def stacked_LSTM(X_test, y_test):
+    ## CNN LSTM
+    model = Sequential()
+    model.add(LSTM(100, return_sequences=True, input_shape=(1, X_test.shape[2]))) # 32 # 50
+    model.add(LSTM(200, return_sequences=True)) #32
+    model.add(LSTM(200)) # 32 # 200
+    model.add(Dense(y_test.shape[1], activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+    
+def CNN(X_train, y_test):
+    ## CNN
+    model = Sequential()
+    #model.add(Conv1D(input_shape=(X_train.shape[1], 1), filters=5, kernel_size=1, activation='relu'))
+    model.add(Conv1D(input_shape=(X_train.shape[1], 1), filters=8, kernel_size=3, activation='relu'))
+    model.add(MaxPooling1D(pool_size=2))
+    model.add(Flatten())
+    model.add(Dense(y_test.shape[1], activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    #model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    #model.compile(loss=losses.categorical_crossentropy, optimizer=optimizers.SGD(lr=0.01), metrics=['accuracy'])
+    #print(model.summary())
+    #history = model.fit(X_train, y_train, batch_size, epochs)
+    return model
+
+def CNN_LSTM(X_train, y_test, dropoutRate):
+    ## CNN LSTM
+    model = Sequential()
+    model.add(Conv1D(input_shape=(X_train.shape[1], 1), filters=8, kernel_size=3, padding='same', activation='relu'))
+    model.add(MaxPooling1D(pool_size=2))
+    model.add(LSTM(200)) # 256
+    model.add(Dropout(dropoutRate))
+    model.add(Dense(y_test.shape[1], activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    #model.compile(loss='categorical_crossentropy', optimizer='adam')
+    #model.compile(loss=losses.categorical_crossentropy, optimizer=optimizers.SGD(lr=0.01), metrics=['accuracy'])
+    #print(model.summary())
+    #history = model.fit(X_train, y_train, batch_size, epochs)
+    return model
+
+def LSTM_with_dropout(X_train, y_test, dropout_rate):
+    ## stacked LSTM
+    model = Sequential()
+    model.add(LSTM(100, input_shape=(X_train.shape[1],X_train.shape[2]), return_sequences=True)) #33
+    #model.add(LSTM(32, return_sequences=True, input_shape=(1, x_test.shape[2])))
+    model.add(Dropout(dropout_rate))
+    #model.add(LSTM(100))
+    #model.add(Dropout(dropout_rate))
+    model.add(Flatten())
+    model.add(Dense(y_test.shape[1], activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    #print(model.summary())
+    #history = model.fit(X_train, y_train, batch_size, epochs)
+    return model
+
+def basic_LSTM(X_train, y_test):
+    model = Sequential()
+    model.add(LSTM(100, input_shape=(X_train.shape[1],X_train.shape[2]), return_sequences=True)) # 33
+    model.add(Flatten())
+    model.add(Dense(y_test.shape[1], activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    #print(model.summary())
+    #history = model.fit(X_train, y_train, batch_size, epochs)
+    return model
+
+def Bidirectional_LSTM(X_train, y_test):
+    model = Sequential()
+    model.add(Bidirectional(LSTM(100, return_sequences=True), input_shape=(X_train.shape[1],X_train.shape[2])))
+    model.add(Flatten())
+    model.add(Dense(y_test.shape[1], activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+# define baseline model
+def DNN(X_train, Y_train, dropout):
+	# create model
+    model = Sequential() 
+    model.add(Dense(20, input_dim=X_train.shape[1], init='normal', activation='relu')) # 20
+    model.add(Dropout(dropout))
+    model.add(Dense(100, init='normal', activation='relu')) # 100
+    model.add(Dropout(dropout))
+    model.add(Dense(100, init='normal', activation='relu')) # 100
+    model.add(Dropout(dropout))
+    #model.add(Dense(200, init='normal', activation='relu'))
+    #model.add(Dropout(dropout))
+    #model.add(Dense(200, init='normal', activation='relu'))
+    #model.add(Dropout(dropout))
+    #sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+    model.add(Dense(Y_train.shape[1], init='normal', activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    #model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+    return model
+
+def getAccuracy(prediction, y_test): ### prediction and y_test are both encoded.
+    sample_size = prediction.shape[0]
+    col_num = prediction.shape[1]
+    correct_num = 0
+    wrong_num = 0
+    tp = 0
+    tn = 0
+    fp = 0
+    fn = 0
+    for i in range(sample_size):
+        cur_row = prediction[i,:]
+        max = 0
+        max_id = 0
+        res_id = 0 
+        for j in range(col_num):
+            if cur_row[j] > max:
+                max = cur_row[j]
+                max_id = j
+        for k in range(col_num):
+            if y_test[i, k] == 1:
+                res_id = k
+                break
+        if res_id == max_id:
+            #print("result id")
+            #print(res_id)
+            correct_num = correct_num + 1
+            if res_id == 1:
+                tp = tp + 1
+            else:
+                tn = tn + 1
+        else:
+            wrong_num = wrong_num + 1
+            if res_id == 0:
+                fn = fn + 1
+            else:
+                fp = fp + 1
+    accuracy = float(correct_num) / sample_size
+    precision = float(tp) / (tp + fp)
+    recall = float(tp) / (tp + fn)
+    TNR = float(tn) / (tn + fp)
+    F = 2 * float(precision * recall) / (precision + recall) 
+    return accuracy, precision, recall, TNR, F
+
+def getAccuracyMulti(y_pred, y_true):
+    accuracy = accuracy_score(y_true, y_pred)
+    F1 = metrics.f1_score(y_true, y_pred, average='weighted') 
+    precision = metrics.precision_score(y_true, y_pred, average='weighted')
+    recall = metrics.recall_score(y_true, y_pred, average='weighted')
+    fbeta = metrics.fbeta_score(y_true, y_pred, average='weighted', beta=0.5)
+    return accuracy, precision, recall, F1, fbeta
+
+def transformResult(result):
+    sample_size = result.shape[0]
+    col_num = result.shape[1]
+    result_list = list()
+    for i in range(sample_size):
+        cur_row = result[i,:]
+        max = 0
+        max_id = 0
+        for j in range(col_num):
+            if cur_row[j] > max:
+                max = cur_row[j]
+                max_id = j
+        result_list.append(max_id)
+    return result_list
+
+def plotTrainingAccuracy(history):
+    plt.plot(history.history['acc'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train'], loc='upper left')
+    plt.show()
+    
+def plotTrainingLoss(history):
+    plt.plot(history.history['loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train'], loc='upper left')
+    plt.show()
+    
+def plotRoc(y_predict, y_label):
+    # roc for unigram
+    fprUni, tprUni, _ = roc_curve(y_predict, y_label)
+    roc_aucUni = auc(fprUni, tprUni)
+    
+    plt.figure()
+    lw = 2
+    plt.plot(fprUni, tprUni, color='darkorange',
+             lw=lw, label='ROC curve (AUC = %0.2f)' % roc_aucUni)
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic')
+    plt.legend(loc="lower right")
+    plt.show()
+    
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
---
+++ stacked_models.py
@@ -0,0 +1,254 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  2 10:21:08 2019
+
+@author: xiaodanli
+"""
+
+import numpy as np
+from sklearn.model_selection import StratifiedKFold
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+import xgboost as xgb
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.linear_model import LogisticRegression
+import pandas as pd
+from keras.utils import np_utils
+from sklearn.model_selection import train_test_split
+from deep_learning_models import stacked_LSTM, CNN, CNN_LSTM, LSTM_with_dropout, basic_LSTM, Bidirectional_LSTM, DNN, transformResult, getAccuracyMulti
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import MinMaxScaler 
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import LinearSVC
+
+def stacking(train_x, train_y, test_x, test_y, epochs, batch_size):
+
+    """ stacking
+
+    input: train_x, train_y, test
+
+    output: test的预测值
+
+    clfs: 5个一级分类器
+
+    dataset_blend_train: 一级分类器的prediction, 二级分类器的train_x
+
+    dataset_blend_test: 二级分类器的test
+
+    """
+    labels = pd.Series(test_y['label'])
+    labels = labels.convert_objects(convert_numeric=True)
+    y_test_onehot = np_utils.to_categorical(labels)
+    
+    labels = pd.Series(train_y['label'])
+    labels = labels.convert_objects(convert_numeric=True)
+    y_train_onehot = np_utils.to_categorical(labels)
+    
+    ### adjust the dataset dimension
+    # reshape X to be [samples, time steps, features]
+    X_test_LSTM = np.reshape(test_x, (test_x.shape[0], 1, test_x.shape[1]))
+    
+    #epochs = 13
+    #batch_size = 250
+    
+    ### Stacked LSTM
+    sLSTM_params = {
+        'batch_size' : batch_size,
+        'epochs' : epochs #15
+    }
+    
+    clf_sLSTM= stacked_LSTM(X_test_LSTM, y_test_onehot)
+    
+    ### input for CNN
+    X_train_CNN = np.reshape(train_x, (train_x.shape[0], train_x.shape[1], 1))
+    X_test_CNN = np.reshape(test_x, (test_x.shape[0], test_x.shape[1], 1))
+    
+    ### CNN
+    CNN_params = {
+        'batch_size' : batch_size,
+        'epochs' : epochs #15
+    }
+    
+    clf_cnn= CNN(X_train_CNN, y_test_onehot)
+
+    ### CNN LSTM
+    CNN_LSTM_params = {
+        'batch_size' : batch_size,
+        'epochs' : epochs # 15
+    }
+    dropout = 0.2
+    clf_cnn_lstm= CNN_LSTM(X_train_CNN, y_test_onehot, dropout)
+    
+    ### LSTM_with_dropout
+    LSTM_dropout_params = {
+        'batch_size' : batch_size,
+        'epochs' : epochs #15
+    }
+    clf_lstm_dropout = LSTM_with_dropout(X_test_LSTM, y_test_onehot, dropout)
+    
+    ### LSTM
+    LSTM_params = {
+        'batch_size' : batch_size,
+        'epochs' : epochs #15
+    }
+    
+    clf_LSTM= basic_LSTM(X_test_LSTM, y_test_onehot)
+    
+    ### bidirectional LSTM
+    biLSTM_params = {
+        'batch_size' : batch_size,
+        'epochs' : epochs #15
+    }
+    clf_biLSTM= Bidirectional_LSTM(X_test_LSTM, y_test_onehot)
+    
+    ### DNN
+    DNN_params = {
+        'batch_size' : batch_size,
+        'epochs' : epochs #15
+    }
+    clf_dnn = DNN(train_x, y_train_onehot, dropout)
+    # 5个一级分类器
+    clfs = [#SVC(C = 3, kernel="rbf"),
+            OneVsRestClassifier(LinearSVC(random_state=0)),
+            RandomForestClassifier(n_estimators=100, max_features="log2", max_depth=10, min_samples_leaf=1, bootstrap=True, n_jobs=-1, random_state=1),
+            KNeighborsClassifier(n_neighbors=15, n_jobs=-1),
+            xgb.XGBClassifier(n_estimators=100, objective="multi:softmax", gamma=1, max_depth=10, subsample=0.8, nthread=-1, seed=1, num_class=3),
+            ExtraTreesClassifier(n_estimators=100, criterion="gini", max_features="log2", max_depth=10, min_samples_split=2, min_samples_leaf=1,bootstrap=True, n_jobs=-1, random_state=1),
+            clf_sLSTM,
+            clf_cnn_lstm,
+            clf_dnn
+            ]
+
+    # 二级分类器的train_x, test
+    dataset_blend_train = np.zeros((train_x.shape[0], len(clfs)), dtype=np.int)
+    dataset_blend_test = np.zeros((test_x.shape[0], len(clfs)), dtype=np.int)
+    # 5个分类器进行8_folds预测
+
+    n_folds = 8 # 8
+
+    skf = StratifiedKFold(n_splits=n_folds, shuffle=True, random_state=1)
+
+    for i, clf in enumerate(clfs):
+
+        dataset_blend_test_j = np.zeros((test_x.shape[0], n_folds))  # 每个分类器的单次fold预测结果
+
+        for j,(train_index,test_index) in enumerate(skf.split(train_x, train_y)):
+            tr_x = train_x[train_index]
+            tr_y = train_y.to_numpy()[train_index]
+            if i == 5:
+                # reshape X to be [samples, time steps, features]
+                tr_x = np.reshape(tr_x, (tr_x.shape[0], 1, tr_x.shape[1]))
+                tr_y = np_utils.to_categorical(tr_y)
+                clf.fit(tr_x, tr_y, sLSTM_params['batch_size'], sLSTM_params['epochs'])
+                tmp = train_x[test_index]
+                tmp_1 = np.reshape(tmp, (tmp.shape[0], 1, tmp.shape[1]))
+                sLSTM_prediction = clf.predict(tmp_1)
+                dataset_blend_train[test_index, i] = transformResult(sLSTM_prediction)
+                X_test_LSTM = np.reshape(test_x, (test_x.shape[0], 1, test_x.shape[1]))
+                dataset_blend_test_j[:, j] = transformResult(clf.predict(X_test_LSTM))
+            elif i == 6:
+                tr_x = np.reshape(tr_x, (tr_x.shape[0], tr_x.shape[1], 1))
+                tr_y = np_utils.to_categorical(tr_y)
+                clf.fit(tr_x, tr_y, CNN_LSTM_params['batch_size'], CNN_LSTM_params['epochs'])
+                tmp = train_x[test_index]
+                tmp_2 = np.reshape(tmp, (tmp.shape[0], tmp.shape[1], 1))
+                CNN_LSTM_prediction = clf.predict(tmp_2)
+                dataset_blend_train[test_index, i] = transformResult(CNN_LSTM_prediction)  
+                X_test_CNN_LSTM = np.reshape(test_x, (test_x.shape[0], test_x.shape[1], 1))
+                dataset_blend_test_j[:, j] = transformResult(clf.predict(X_test_CNN_LSTM))
+            elif i == 7:
+                tr_y = np_utils.to_categorical(tr_y)
+                clf.fit(tr_x, tr_y, DNN_params['batch_size'], DNN_params['epochs'])
+                dataset_blend_train[test_index, i] = transformResult(clf.predict(train_x[test_index]))
+                dataset_blend_test_j[:, j] = transformResult(clf.predict(test_x))
+            else:
+                clf.fit(tr_x, tr_y)
+                dataset_blend_train[test_index, i] = clf.predict(train_x[test_index])
+                dataset_blend_test_j[:, j] = clf.predict(test_x)
+
+        dataset_blend_test[:, i] = dataset_blend_test_j.sum(axis=1) // (n_folds//2 + 1)
+
+    # 二级分类器进行预测
+
+    clf = LogisticRegression(penalty="l2", tol=1e-6, C=1.0, random_state=1, n_jobs=-1, multi_class='multinomial', solver='newton-cg')
+
+    clf.fit(dataset_blend_train, train_y)
+    
+    prediction = clf.predict(dataset_blend_test)
+    
+    return prediction, dataset_blend_train, dataset_blend_test
+
+if __name__ == "__main__":
+    df = pd.read_csv('all_features_with_label.csv', sep=',',header=None) 
+    df_feature = df.iloc[:, 1:]
+    X = df_feature.values
+    label = df.iloc[:,0]
+    y = label.values
+    cols = list(df)
+    columns_names = ["label"]
+    for i in range(0, 1817):
+        columns_names.append("Column " + str(i))
+    df.columns = columns_names
+    """
+        DATA EXPLORATION ANALYSIS
+    """
+    #correlation matrix
+    #k = 15 #number of variables for heatmap
+    #corrmat = df.corr()
+    #cols = corrmat.nlargest(k, 'label')['label'].index
+    #cm = np.corrcoef(df[cols].values.T)
+    #sns.set(font_scale=1.25)
+    #hm = sns.heatmap(cm, cbar=True, annot=True, square=True, fmt='.2f', annot_kws={'size': 10}, yticklabels=cols.values, xticklabels=cols.values)
+    #plt.show()
+    
+    #scatterplot
+    #sns.set()
+    #sns.pairplot(df[cols], size = 2.5)
+    #plt.show()
+    
+    #missing data
+    total = df_feature.isnull().sum().sort_values(ascending=False)
+    percent = (df_feature.isnull().sum()/df_feature.isnull().count()).sort_values(ascending=False)
+    missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
+    #print(missing_data.head(20))
+    
+    threshold = 0.80
+    corr_matrix = df[df['label'].notnull()].corr().abs()
+    upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k = 1).astype(np.bool))
+    to_drop = [column for column in upper.columns if any(upper[column] >= threshold)]
+    df_removed = df.drop(columns = to_drop)
+    
+    X = df_removed.drop(columns = ['label'])
+    y = df_removed[['label']]
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+    
+    scaler = MinMaxScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
+    best_acc = 0
+    best_epo = 0
+    best_size = 0
+    
+    for epochs in range(10, 16, 1):
+        for batch_size in range(200, 500, 50):
+            prediction, dataset_blend_train, dataset_blend_test = stacking(X_train, y_train, X_test, y_test, epochs, batch_size)
+            accuracy, precision, recall, F1, fbeta = getAccuracyMulti(prediction, y_test)
+            print(epochs)
+            print(batch_size)
+            print(accuracy)    
+            print(precision)    
+            print(recall)    
+            print(F1)    
+            print(fbeta)
+            if accuracy > best_acc:
+                best_acc = accuracy
+                best_epo = epochs
+                best_size = batch_size
+      
+    print("final output")    
+    print(best_acc)
+    print(best_epo)
+    print(best_size)
